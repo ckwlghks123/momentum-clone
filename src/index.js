61d488dc67cd5d457b66e604c99dbd33b49ae34e
Ji <ji@Jiui-MacBookPro.local>
2021-05-03 13:54:39 +0900

    modified

--- src/index.js
+++ src/index.js
@@ -1,90 +1,90 @@
-const input = document.querySelector("input");
-const unDone = document.querySelector(".not-done");
-const done = document.querySelector(".done");
-const body = document.querySelector("body");
-let unDoneTodos = [];
-let doneTodos = [];
-let id = 0;
-const reUnDone = JSON.parse(localStorage.getItem("unDoneTodos"));
-const reDone = JSON.parse(localStorage.getItem("doneTodos"));
+// const input = document.querySelector("input");
+// const unDone = document.querySelector(".not-done");
+// const done = document.querySelector(".done");
+// const body = document.querySelector("body");
+// let unDoneTodos = [];
+// let doneTodos = [];
+// let id = 0;
+// const reUnDone = JSON.parse(localStorage.getItem("unDoneTodos"));
+// const reDone = JSON.parse(localStorage.getItem("doneTodos"));
 
-function filterArr(arr, li, eq) {
-  if (eq) {
-    return arr.filter((todo) => todo.id === parseInt(li.id));
-  } else {
-    return arr.filter((todo) => todo.id !== parseInt(li.id));
-  }
-}
+// function filterArr(arr, li, eq) {
+//   if (eq) {
+//     return arr.filter((todo) => todo.id === parseInt(li.id));
+//   } else {
+//     return arr.filter((todo) => todo.id !== parseInt(li.id));
+//   }
+// }
 
-function addList(text) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const delBtn = document.createElement("button");
-  const swBtn = document.createElement("button");
-  const temTodo = {
-    id: id,
-    text: text,
-    done: false,
-  };
-  li.id = id;
-  span.innerHTML = text;
-  delBtn.innerHTML = "삭제";
-  delBtn.addEventListener("click", (e) => {
-    const li = e.target.parentNode;
-    if (li.parentNode === unDone) {
-      unDoneTodos = filterArr(unDoneTodos, li);
-      li.parentNode.removeChild(li);
-      localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
-    } else {
-      doneTodos = filterArr(doneTodos, li);
-      li.parentNode.removeChild(li);
-      localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
-    }
-  });
-  swBtn.innerHTML = "변환";
-  swBtn.addEventListener("click", (e) => {
-    const li = e.target.parentNode;
-    if (li.parentNode === unDone) {
-      console.log(li);
-      const task = filterArr(unDoneTodos, li, true);
-      unDoneTodos = filterArr(unDoneTodos, li);
-      task.forEach((todo) => (todo.done = true));
-      done.appendChild(li);
-      doneTodos.push(task[0]);
-      localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
-      localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
-    } else if (li.parentNode === done) {
-      const task = filterArr(doneTodos, li, true);
-      doneTodos = filterArr(unDoneTodos, li);
-      task.forEach((todo) => (todo.done = false));
-      unDone.appendChild(li);
-      unDoneTodos.push(task[0]);
-      localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
-      localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
-    }
-  });
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.appendChild(swBtn);
-  unDone.appendChild(li);
-  unDoneTodos.push(temTodo);
-  localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
-}
+// function addList(text) {
+//   const li = document.createElement("li");
+//   const span = document.createElement("span");
+//   const delBtn = document.createElement("button");
+//   const swBtn = document.createElement("button");
+//   const temTodo = {
+//     id: id,
+//     text: text,
+//     done: false,
+//   };
+//   li.id = id;
+//   span.innerHTML = text;
+//   delBtn.innerHTML = "삭제";
+//   delBtn.addEventListener("click", (e) => {
+//     const li = e.target.parentNode;
+//     if (li.parentNode === unDone) {
+//       unDoneTodos = filterArr(unDoneTodos, li);
+//       li.parentNode.removeChild(li);
+//       localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
+//     } else {
+//       doneTodos = filterArr(doneTodos, li);
+//       li.parentNode.removeChild(li);
+//       localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
+//     }
+//   });
+//   swBtn.innerHTML = "변환";
+//   swBtn.addEventListener("click", (e) => {
+//     const li = e.target.parentNode;
+//     if (li.parentNode === unDone) {
+//       console.log(li);
+//       const task = filterArr(unDoneTodos, li, true);
+//       unDoneTodos = filterArr(unDoneTodos, li);
+//       task.forEach((todo) => (todo.done = true));
+//       done.appendChild(li);
+//       doneTodos.push(task[0]);
+//       localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
+//       localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
+//     } else if (li.parentNode === done) {
+//       const task = filterArr(doneTodos, li, true);
+//       doneTodos = filterArr(unDoneTodos, li);
+//       task.forEach((todo) => (todo.done = false));
+//       unDone.appendChild(li);
+//       unDoneTodos.push(task[0]);
+//       localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
+//       localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
+//     }
+//   });
+//   li.appendChild(span);
+//   li.appendChild(delBtn);
+//   li.appendChild(swBtn);
+//   unDone.appendChild(li);
+//   unDoneTodos.push(temTodo);
+//   localStorage.setItem("unDoneTodos", JSON.stringify(unDoneTodos));
+// }
 
-function onSubmit(e) {
-  if (e.keyCode === 13) {
-    const inputText = e.target.value;
+// function onSubmit(e) {
+//   if (e.keyCode === 13) {
+//     const inputText = e.target.value;
 
-    if (inputText) {
-      id++;
-      addList(inputText);
-    }
-    e.target.value = "";
-  }
-}
+//     if (inputText) {
+//       id++;
+//       addList(inputText);
+//     }
+//     e.target.value = "";
+//   }
+// }
 
-function init() {
-  input.addEventListener("keyup", onSubmit);
-}
+// function init() {
+//   input.addEventListener("keyup", onSubmit);
+// }
 
-init();
+// init();
--- src/script/weather.js
+++ src/script/weather.js
@@ -5,74 +5,65 @@ const displayCity = document.querySelector('.weather__city');
 const displayHumidity = document.querySelector('.weather__humidity');
 const displayDesc = document.querySelector('.weather__desc');
 
-
 const locations = 'coords';
-const API_KEY = 'a4fa961997fb534248cc4d6e3cd4f58b'
+const API_KEY = 'a4fa961997fb534248cc4d6e3cd4f58b';
 
-// 아이콘 참고 >>>> 
-http://erikflowers.github.io/weather-icons/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
-https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+// 아이콘 참고 >>>>
+//erikflowers.github.io/weather-icons/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
+//openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 // icon 상태코드값 참조해서 아이콘 변경
 
-function paintWeather({temp, city,humidity,icon,description}){
-  // console.log(temp,city,humidity,icon,description);
-  displayTemp.innerHTML = `${temp.toFixed(1)} <i class="wi wi-celsius"></i>`
-  displayIcon.innerHTML = `<i class="wi wi-night-clear"></i>`
-  displayCity.innerText = city
-}
-
-async function getWeather(lat, lng){
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`)
-  const data = await res.json()
-
-  const {main,name,weather} = data
-  const {temp,humidity} = main
-  const {id,description} = weather[0]
-  const weatherInfo = {
-    city: name,
-    icon: id,
+http: https: function paintWeather({
     temp,
+    city,
     humidity,
+    icon,
     description,
-  }
-  paintWeather(weatherInfo)
+}) {
+    // console.log(temp,city,humidity,icon,description);
+    displayTemp.innerHTML = `${temp.toFixed(1)} <i class="wi wi-celsius"></i>`;
+    displayIcon.innerHTML = `<i class="wi wi-night-clear"></i>`;
+    displayCity.innerText = city;
 }
 
-function getSuccess(position){
-  const {latitude,longitude} = position.coords
-  const coObj = {
-    latitude,
-    longitude
-  }
-  saveCoords(coObj)
+async function getWeather(lat, lng) {
+    const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`
+    );
+    const data = await res.json();
+
+    const { main, name, weather } = data;
+    const { temp, humidity } = main;
+    const { id, description } = weather[0];
+    const weatherInfo = {
+        city: name,
+        icon: id,
+        temp,
+        humidity,
+        description,
+    };
+    paintWeather(weatherInfo);
 }
-function saveCoords(obj){
-  localStorage.setItem(locations,JSON.stringify(obj))
+
+function getSuccess(position) {
+    const { latitude, longitude } = position.coords;
+    const coObj = {
+        latitude,
+        longitude,
+    };
+    localStorage.setItem(locations, JSON.stringify(coObj));
+    getWeather(latitude, longitude);
 }
-function getError(e){
-  console.log(e.message)
+function getError(e) {
+    console.log(e.message);
 }
 
 function getCoords() {
-  navigator.geolocation.getCurrentPosition(getSuccess,getError)
+    navigator.geolocation.getCurrentPosition(getSuccess, getError);
 }
 
-function loadCoords(){
-  const loadedCoords = localStorage.getItem(locations);
-  if (loadedCoords === null){
+function init() {
     getCoords();
-  } else {
-    const {latitude,longitude} = JSON.parse(loadedCoords)
-    // const lat = Math.floor(latitude)
-    // const long = Math.floor(longitude)
-    // getWeather(lat,long)
-    getWeather(latitude,longitude)
-  }
-  
-}
-
-function init(){
-  loadCoords()
 }
 
 init();
